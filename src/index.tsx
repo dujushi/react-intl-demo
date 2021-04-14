4face2ae0057c5b954a8b36ad8f514dce1a9bdef
William Guo <dujushi@gmail.com>
2021-04-14 22:53:55 +1200

    load translations

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,15 @@ import { IntlProvider } from "react-intl";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const locale = 'fr';
+import(`./lang/${locale}.json`).then(messages => ReactDOM.render(
   <React.StrictMode>
-    <IntlProvider locale={"en"} defaultLocale="en" messages={{}}>
+    <IntlProvider locale={locale} defaultLocale="en" messages={messages}>
       <App />
     </IntlProvider>
   </React.StrictMode>,
   document.getElementById("root")
-);
+));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
