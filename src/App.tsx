90abb55ac44a15db175f2c3ecaa5ea9ad2304e6e
William Guo <dujushi@gmail.com>
2021-04-16 09:55:39 +1200

    add a select message

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,25 @@ function App() {
         })}
       </p>
       <p>
-        {formatMessage({
-          defaultMessage: "Hello {who}",
-        }, { who: 'World'})}
+        {formatMessage(
+          {
+            defaultMessage: "Hello {who}",
+          },
+          { who: "World" }
+        )}
+      </p>
+      <p>
+        {formatMessage(
+          {
+            defaultMessage: `
+{gender, select,
+    male {He}
+    female {She}
+    other {They}
+} will respond shortly.`,
+          },
+          { gender: "female" }
+        )}
       </p>
     </div>
   );
